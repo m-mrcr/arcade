a7a2cee8b1d2b5e375a12de4c6d4f796b8a64b60
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-08-10 17:14:18 -0600

    re-adds indexRouter

--- app.js
+++ app.js
@@ -13,5 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/api/v1/games', gamesRouter)
 module.exports = app;
